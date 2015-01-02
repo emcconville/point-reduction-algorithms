631ea2af31f1001f14f6d69976bc248e677c1118
Eric McConville <emcconville@emcconville.com>
2015-01-02 06:35:33 -0600

    Fixed some minor issues created by PHPCS, and CI tools

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-point-reduction-algorithms
+Point Reduction Algorithms
 ==========================
 
 A collection of algorithms for reducing the number of points in polyline
+
+[![Build Status](https://travis-ci.org/emcconville/point-reduction-algorithms.svg?branch=master)](https://travis-ci.org/emcconville/point-reduction-algorithms)
+[![Coverage Status](https://img.shields.io/coveralls/emcconville/point-reduction-algorithms.svg)](https://coveralls.io/r/emcconville/point-reduction-algorithms?branch=master)
--- src/Common/Math.php
+++ src/Common/Math.php
@@ -12,17 +12,19 @@ class Math
     {
         $return = 0.0;
         $length = self::distanceBetweenPoints($h, $t);
-        if ($length == 0) { return sqrt(self::distanceBetweenPoints($p, $h)); 
+        if ($length == 0)
+        {
+            return sqrt(self::distanceBetweenPoints($p, $h));
         }
-        $d = (
-          ( $p->x - $h->x ) * ( $t->x - $h->x )
-          + ($p->y - $h->y ) * ( $t->y - $h->y )
-         ) / $length;
-        if ( $d < 0 ) { $return = self::distanceBetweenPoints($p, $h); 
-        }
-        elseif ( $d > 1 ) { $return = self::distanceBetweenPoints($p, $t); 
-        }
-        else {
+        $d  = ( $p->x - $h->x ) * ( $t->x - $h->x );
+        $d += ( $p->y - $h->y ) * ( $t->y - $h->y );
+        $d /= $length;
+        if ( $d < 0 )
+        {
+            $return = self::distanceBetweenPoints($p, $h);
+        } elseif ( $d > 1 ) {
+            $return = self::distanceBetweenPoints($p, $t);
+        } else {
             $return = self::distanceBetweenPoints(
                 $p,
                 new Point(
