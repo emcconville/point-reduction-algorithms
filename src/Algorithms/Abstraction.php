4e06a69e25cf2bed025c7912a1bf0596c751016f
jmleroux <jmleroux.pro@gmail.com>
2016-10-28 01:14:28 +0200

    Minor fixes: * unused namespaces * __construct does not return in PHP * unused variables * PHPDoc

--- src/Algorithms/Abstraction.php
+++ src/Algorithms/Abstraction.php
@@ -53,8 +53,6 @@ abstract class Abstraction implements \Countable
      * Allocate & Initialize sub-class
      *
      * @param array $points Optional subject points.
-     *
-     * @return Abstraction subclass
      */
     public function __construct( $points = null )
     {
@@ -69,11 +67,11 @@ abstract class Abstraction implements \Countable
      *
      * @param mixed $points Must be array or Traversable
      *
-     * @return null
+     * @throws InvalidArgumentException
      */
     public function setPoints( $points )
     {
-        if ( is_array($points) || $points instanceof Traversable ) {
+        if ( is_array($points) || $points instanceof \Traversable ) {
             array_walk(
                 $points,
                 function (&$point, $index) {
@@ -101,9 +99,9 @@ abstract class Abstraction implements \Countable
     /**
      * Calculate the area of a triangle
      *
-     * @param Point $a First point
-     * @param Point $b Middle point
-     * @param Point $c Last point
+     * @param PointInterface $a First point
+     * @param PointInterface $b Middle point
+     * @param PointInterface $c Last point
      *
      * @return float
      */
@@ -134,8 +132,8 @@ abstract class Abstraction implements \Countable
     /**
      * Calculate the distance between to points
      *
-     * @param Point $head First point
-     * @param Point $tail Last point
+     * @param PointInterface $head First point
+     * @param PointInterface $tail Last point
      *
      * @return float
      */
@@ -204,7 +202,6 @@ abstract class Abstraction implements \Countable
         list( $ax, $ay ) = $a->getCoordinates();
         list( $bx, $by ) = $b->getCoordinates();
         list( $cx, $cy ) = $c->getCoordinates();
-        $return = 0.0;
         $length = self::pythagorus($b, $c);
         if ( $length == 0 ) {
             return self::distanceBetweenPoints($a, $b);
